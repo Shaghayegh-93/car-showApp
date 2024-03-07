d898fe16704420eb7bad7bd8aa6b412677d546cc
Shaghayegh Hosseini <shaghayegh.hoseini93@gmail.com>
2024-03-07 12:34:20 +0000

    add  global app style

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <main className="overflow-hidden"></main>;
+  return <main className="overflow-hidden">
+    <Hero/>
+  </main>;
 }
